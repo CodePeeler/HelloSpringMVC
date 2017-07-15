88d271832e41c201fff763339b011fbdd14b9b55
CodePeeler <sdornan1729@gmail.com>
2017-07-15 15:50:24 +0100

    Add files via upload

---
+++ src/main/java/org/o7planning/hellospringmvc/controller/HelloWorldController.java
@@ -0,0 +1,41 @@
+package org.o7planning.hellospringmvc.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+ 
+@Controller
+public class HelloWorldController {
+ 
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        
+        model.addAttribute("greeting", "Hello Spring4 MVC");
+        
+        return "helloworld";
+        
+    }
+   
+    @RequestMapping("/user")
+    public String userInfo(Model model, @RequestParam(value = "name", defaultValue = "Guest") String name) {
+    	
+    	model.addAttribute("name", name);
+    	
+    	if ("admin".equals(name)) {
+    		model.addAttribute("email", "admin@example.com");
+    	} else {
+    		model.addAttribute("email", "Not set");
+    	}
+    	
+    	return "userinfo";
+    }
+    
+    @RequestMapping(value = "/gotohello", method = RequestMethod.GET)
+    public String authorInfo(Model model) {
+         
+        return "redirect:/hello";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/pages/helloworld.jsp
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!-- If you are using the old JSP 1.2 descriptor, defined by DTD ,for example in web.xml -->
+<!-- then you gotta add the taglib i.e. lines 4 and 8 -->
+<!-- %@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" % -->
+<html>
+<head>
+<!--  -->
+<!-- %@ page isELIgnored="false" % -->
+<meta charset="UTF-8">
+<title>Spring MVC -HelloWorld</title>
+</head>
+ 
+ 
+<body>
+    <h1>${greeting}</h1>
+    <h2>TEST</h2>
+</body>
+ 
+ 
+</html>
